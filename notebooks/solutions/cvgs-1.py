4635e2077199ec05fea8487b3079b54a0a5f5ed5
Matthew Rocklin <mrocklin@gmail.com>
2016-07-11 18:51:36 -0500

    add solutions to cross val

---
+++ notebooks/solutions/cvgs-1.py
@@ -0,0 +1,11 @@
+from concurrent.futures import ThreadPoolExecutor
+e = ThreadPoolExecutor()
+
+futures = []
+
+for split in cv_splits:
+    for params in param_space:
+        future = e.submit(evaluate_one, SVC, params, split)
+        futures.append(future)
+        
+results = [f.result() for f in futures]
