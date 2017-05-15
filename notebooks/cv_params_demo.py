08979f6e4e1efebc9ee2fb0b8ddf10e47c4af359
Matthew Rocklin <mrocklin@gmail.com>
2017-05-15 13:42:23 -0400

    convert more imports to sklearn.model_selection
    This avoids deprecation errors

--- notebooks/cv_params_demo.py
+++ notebooks/cv_params_demo.py
@@ -1,5 +1,5 @@
 from sklearn.datasets import load_digits
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.externals.joblib import hash
 import pandas as pd
 import numpy as np
