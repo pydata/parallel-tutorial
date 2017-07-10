2c6b56b25bd7234f166b5c84743ecf6b6dc922a7
Matthew Rocklin <mrocklin@gmail.com>
2017-07-10 10:13:08 -0400

    minor clean up to single-machine notebooks

--- notebooks/solutions/cvgs-1.py
+++ notebooks/solutions/cvgs-1.py
@@ -3,9 +3,11 @@ e = ThreadPoolExecutor()
 
 futures = []
 
+parameters = list(param_samples)
+
 for split in cv_splits:
-    for params in param_samples:
+    for params in parameters:
         future = e.submit(evaluate_one, SVC, params, split)
         futures.append(future)
-        
+
 results = [f.result() for f in futures]
